93fb5823147f402c05351657859bc71279c71fc2
DanGSun <DanGSun@yandex.ru>
2019-12-21 19:19:44 +0300

    WIP: Attaching nphysics to rendering engine #2

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,57 @@ struct SimulatorWorld {
     force_generators: DefaultForceGeneratorSet<f32>,
 }
 
-fn tick(world: &SimulatorWorld) {}
+fn physics_tick(world: &mut SimulatorWorld) {}
+
+fn init_world(world: &mut SimulatorWorld) {
+    /*
+     * Ground.
+     */
+    let ground_thickness = 0.2;
+    let ground_shape = ShapeHandle::new(Cuboid::new(Vector3::new(3.0, ground_thickness, 3.0)));
+
+    let ground_handle = world.bodies.insert(Ground::new());
+    let co = ColliderDesc::new(ground_shape)
+        .translation(Vector3::y() * -ground_thickness)
+        .build(BodyPartHandle(ground_handle, 0));
+    world.colliders.insert(co);
+
+    /*
+     * Create the boxes.
+     */
+    let num = 6;
+    let rad = 0.1;
+
+    let cuboid = ShapeHandle::new(Cuboid::new(Vector3::repeat(rad)));
+
+    let shift = (rad + ColliderDesc::<f32>::default_margin()) * 2.0;
+    let centerx = shift * (num as f32) / 2.0;
+    let centery = shift / 2.0;
+    let centerz = shift * (num as f32) / 2.0;
+    let height = 3.0;
+
+    for i in 0usize..num {
+        for j in 0usize..num {
+            for k in 0usize..num {
+                let x = i as f32 * shift - centerx;
+                let y = j as f32 * shift + centery + height;
+                let z = k as f32 * shift - centerz;
+
+                // Build the rigid body.
+                let rb = RigidBodyDesc::new()
+                    .translation(Vector3::new(x, y, z))
+                    .build();
+                let rb_handle = world.bodies.insert(rb);
+
+                // Build the collider.
+                let co = ColliderDesc::new(cuboid.clone())
+                    .density(1.0)
+                    .build(BodyPartHandle(rb_handle, 0));
+                world.colliders.insert(co);
+            }
+        }
+    }
+}
 
 fn main() {
     // nphysics' world init
@@ -44,7 +94,16 @@ fn main() {
 
     world
         .geometrical_world
-        .maintain(&mut world.bodies, &mut world.colliders);
+        .maintain(&mut world.bodies, &mut world.colliders); // TODO: Move to SimulatorWorld impl
+
+    world.mechanical_world.maintain(
+        &mut world.geometrical_world,
+        &mut world.bodies,
+        &mut world.colliders,
+        &mut world.joint_constraints,
+    );
+
+    // world.mechanical_world.counters.enable();
 
     let mut win = three::Window::new("Helios Engine");
     let cam = win.factory.perspective_camera(45.0, 1.0..150.0);
@@ -62,6 +121,8 @@ fn main() {
 
     let mut is_fullscreen = false;
     let mut is_camera_moved = true;
+
+    init_world(&mut world);
 
     while win.update() && !win.input.hit(three::KEY_ESCAPE) {
         if is_camera_moved {
@@ -84,6 +145,21 @@ fn main() {
             win.set_fullscreen(is_fullscreen)
         }
 
+        world.mechanical_world.step(
+            &mut world.geometrical_world,
+            &mut world.bodies,
+            &mut world.colliders,
+            &mut world.joint_constraints,
+            &mut world.force_generators,
+        );
+
+        physics_tick(&mut world);
         win.render(&cam);
     }
+    println!("Exiting now.");
+    let mut body_counter: i32 = 0;
+    for i in world.bodies.iter() {
+        body_counter += 1;
+    }
+    println!("Amount of bodies, simulated this time: {}", body_counter)
 }
