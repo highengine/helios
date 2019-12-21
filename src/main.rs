c07729c121dd92b193a46773f8f447f95ab48418
DanGSun <DanGSun@yandex.ru>
2019-12-21 15:59:38 +0300

    Commit 2

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,84 @@
 extern crate three;
 
-use three::{Object};
 use three::light::Light;
+use three::Object;
 
 extern crate nalgebra as na;
 
-use na::{Point3, Vector3, RealField};
+use na::{Point3, RealField, Vector3};
 use ncollide3d::shape::{ConvexHull, Cuboid, ShapeHandle};
 use nphysics3d::force_generator::DefaultForceGeneratorSet;
 use nphysics3d::joint::DefaultJointConstraintSet;
-use nphysics3d::object::{BodyPartHandle, ColliderDesc, DefaultBodySet, DefaultColliderSet, Ground, RigidBodyDesc, BodySet, ColliderHandle};
+use nphysics3d::object::{
+    BodyPartHandle, BodySet, ColliderDesc, ColliderHandle, DefaultBodySet, DefaultColliderSet,
+    Ground, RigidBodyDesc,
+};
 use nphysics3d::world::{DefaultGeometricalWorld, DefaultMechanicalWorld};
 
 use rand::distributions::{Distribution, Standard};
 use rand::{rngs::StdRng, SeedableRng};
+use three::camera::Camera;
 
-pub fn init_world(world: &mut three::Window,
-                  camera: &mut three::camera::Camera) {
-    /*
-     * World
-     */
-    let mechanical_world: DefaultMechanicalWorld<f32> = DefaultMechanicalWorld::new(Vector3::new(0.0, -9.81, 0.0));
-    let geometrical_world: nphysics3d::world::DefaultGeometricalWorld<f32> = DefaultGeometricalWorld::new();
-    let mut bodies: DefaultBodySet<f32> = DefaultBodySet::new();
-    let mut colliders: DefaultColliderSet<f32> = DefaultColliderSet::new();
-    let joint_constraints: DefaultJointConstraintSet<f32> = DefaultJointConstraintSet::new();
-    let force_generators: DefaultForceGeneratorSet<f32> = DefaultForceGeneratorSet::new();
-
-    /*
-     * Ground
-     */
-    let ground_thickness = 0.2;
-    let ground_shape = ShapeHandle::new(Cuboid::new(Vector3::new(3.0, ground_thickness, 3.0)));
-
-    let ground_handle = bodies.insert(Ground::new());
-    let co = ColliderDesc::new(ground_shape)
-        .translation(Vector3::y() * -ground_thickness)
-        .build(BodyPartHandle(ground_handle, 0));
-    colliders.insert(co);
-
+struct SimulatorWorld {
+    mechanical_world: DefaultMechanicalWorld<f32>,
+    geometrical_world: DefaultGeometricalWorld<f32>,
+    bodies: DefaultBodySet<f32>,
+    colliders: DefaultColliderSet<f32>,
+    joint_constraints: DefaultJointConstraintSet<f32>,
+    force_generators: DefaultForceGeneratorSet<f32>,
 }
 
+fn tick(world: &SimulatorWorld) {}
+
 fn main() {
+    // nphysics' world init
+
+    let mut world = SimulatorWorld {
+        mechanical_world: DefaultMechanicalWorld::new(Vector3::new(0.0, -9.81, 0.0)), // Remake G-Force impl!
+        geometrical_world: DefaultGeometricalWorld::new(),
+        bodies: DefaultBodySet::new(),
+        colliders: DefaultColliderSet::new(),
+        joint_constraints: DefaultJointConstraintSet::new(),
+        force_generators: DefaultForceGeneratorSet::new(),
+    };
+
     let mut win = three::Window::new("@solmann's SimEngine");
-    let cam = win.factory.perspective_camera(45.0, 1.0 .. 150.0);
-    cam.look_at([-4.0, 25.0, 10.0], [5.0, 0.0, 1.0], None);
+    let cam = win.factory.perspective_camera(45.0, 1.0..150.0);
+
+    /*
+     * TODO: Make lighting work
+     */
+
+    // let ambient_light = win.factory.ambient_light(0xffffffff, 0.5);
+    // win.scene.add(&ambient_light);
 
     let mut cam_x: f32 = 0.;
     let mut cam_y: f32 = 0.;
     let mut cam_z: f32 = 0.;
 
+    let mut is_fullscreen = false;
+    let mut is_camera_moved = true;
 
     while win.update() && !win.input.hit(three::KEY_ESCAPE) {
-        cam.look_at([cam_x.clone(), cam_y.clone(), cam_z.clone()],
-                    [5.0, 0.0, 1.0], None);
+        if is_camera_moved {
+            cam.look_at(
+                [cam_x.clone(), cam_y.clone(), cam_z.clone()],
+                [5.0, 0.0, 1.0], //TODO: Attach camera rotation to user's mouse
+                None,
+            );
+            is_camera_moved = false;
+        }
+
+        if win.input.hit(three::KEY_SPACE) {
+            // TODO: Change button; FIXME: Fix flickering
+            if is_fullscreen {
+                is_fullscreen = false
+            } else {
+                is_fullscreen = true
+            }
+            println!("Fullscreen status: {}", is_fullscreen);
+            win.set_fullscreen(is_fullscreen)
+        }
 
         win.render(&cam);
     }
